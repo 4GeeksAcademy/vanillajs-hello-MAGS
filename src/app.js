cef868349f7895cdc6f848f713c6b85bb957c3bf
yenobis1991-collab <yenobis1991@gmail.com>
2026-03-25 22:28:06 +0000

    excuses

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+
+const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+const action = ['ate', 'peed', 'crushed', 'broke'];
+const what = ['my homework', 'my phone', 'the car'];
+const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+
+const generadorExcuses = (list) => {
+  return Math.floor(Math.random() * list.length);
+}
+
+  const iwho = generadorExcuses(who);
+  const iaction = generadorExcuses(action);
+  const iwhat = generadorExcuses(what);
+  const iwhen = generadorExcuses(when);
+
+ document.getElementById("excuses").innerHTML = 
+    `${who[iwho]} ${action[iaction]} ${what[iwhat]} ${when[iwhen]}`;
 };
